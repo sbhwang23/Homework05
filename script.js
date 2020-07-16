4ff36fbac4d60f8fd129c06aef6ae957a4e7a902
Sarah <soobin.sarah.hwang@gmail.com>
2020-07-16 00:19:53 -0700

    added in color coding for time

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+$(document).ready(function() {  
+
 let hoursAm = [9, 10, 11]
 let hoursPm = [12, 1, 2, 3, 4, 5]
 let currentHour = moment().hours();
+let timeFrame = $('.time-block').text();
 
 
-$(document).ready(function() {   
     function clock() {
         let currentDate = (moment().format('MMMM Do YYYY, h:mm:ss a'));
         $('#currentDay').html(currentDate);
@@ -13,16 +15,27 @@ $(document).ready(function() {
     $.each(hoursAm, function(i, time) {
         $('.container').append('<div class= "row hour"> <div class= "col-1 time-block">' + time + ' AM' + '</div>' + '<textarea class= "col-10 description"></textarea>' + '<button class= "col-1 saveBtn">' + 'Save' + '</button> </div>')
     })
-
+    
     $.each(hoursPm, function(i, time) {
         $('.container').append('<div class= "row hour"> <div class= "col-1 time-block">' + time + ' PM' + '</div>' + '<textarea class= "col-10 description"></textarea>' + '<button class= "col-1 saveBtn">' + 'Save' + '</button> </div>')
     })
 
     $('.saveBtn').on('click', function(e) {
         e.preventDefault();
-        let value = $(this).siblings('textarea').val();
+        let textInput = $(this).siblings('textarea').val();
         let time = $(this).siblings('.time-block').text();
-        localStorage.setItem(time,value);
+        localStorage.setItem(time, textInput);
     })
 
-});
+    $('.time-block').each(function() {
+        if (timeFrame < currentHour) {
+            $('.description').addClass('past');
+        } else if (timeFrame === currentHour) {
+            $('.description').removeClass('past');
+            $('.description').addClass('present');
+        } else {
+            $('.description').removeClass('present');
+            $('.description').addClass('future');
+        }
+    })
+})
